e1ae468f9ab772dd1c741b617faed6a0d518aa7c
kim deokhyeon <tosico85@gmail.com>
2020-07-24 22:14:58 +0900

    setting OAuth

--- src/db.js
+++ src/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
 
 const db = mongoose.connection;
 
--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://whispering-wildwood-28575.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback
   )
@@ -27,7 +29,9 @@ passport.use(
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
       //callbackURL: `http://localhost:4000${routes.facebookCallback}`,
-      callbackURL: `https://cowardly-warthog-94.serverless.social${routes.facebookCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://whispering-wildwood-28575.herokuapp.com${routes.facebookCallback}`
+        : `https://cowardly-warthog-94.serverless.social${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"],
     },
